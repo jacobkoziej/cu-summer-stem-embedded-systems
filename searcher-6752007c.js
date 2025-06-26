ea26c5a54c7a68a215297226acc4bdd1c8efa342
jacobkoziej <jacobkoziej@gmail.com>
2025-06-26 21:18:48 +0000

    5acd85ee5e9bc1d17a5db33146d1105d79722366

--- searcher-6752007c.js
+++ searcher-6752007c.js
@@ -524,6 +524,6 @@ window.search = window.search || {};
         document.head.append(script);
     }
 
-    loadScript(path_to_root + 'searchindex-93c41204.js', 'search-index');
+    loadScript(path_to_root + 'searchindex-3cf5239f.js', 'search-index');
 
 })(window.search);
