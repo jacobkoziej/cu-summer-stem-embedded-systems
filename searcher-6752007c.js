0dff95762348862af6ce41ac8fdab74a3616c84f
jacobkoziej <jacobkoziej@gmail.com>
2025-07-10 02:08:12 +0000

    8dc098c98d08bc248ea617b7f8396b13bf5f31ee

--- searcher-6752007c.js
+++ searcher-6752007c.js
@@ -524,6 +524,6 @@ window.search = window.search || {};
         document.head.append(script);
     }
 
-    loadScript(path_to_root + 'searchindex-8699239a.js', 'search-index');
+    loadScript(path_to_root + 'searchindex-1e54e0ae.js', 'search-index');
 
 })(window.search);
