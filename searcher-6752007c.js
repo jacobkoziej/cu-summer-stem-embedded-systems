7b98f301fdb3c1ccc96a562777c89f0bb0a5ca06
jacobkoziej <jacobkoziej@gmail.com>
2025-07-20 20:24:50 +0000

    2bedadf3aaa3d2384e48ffee9fc28eb77f794bf7

--- searcher-6752007c.js
+++ searcher-6752007c.js
@@ -524,6 +524,6 @@ window.search = window.search || {};
         document.head.append(script);
     }
 
-    loadScript(path_to_root + 'searchindex-70bff7bc.js', 'search-index');
+    loadScript(path_to_root + 'searchindex-c98deecc.js', 'search-index');
 
 })(window.search);
