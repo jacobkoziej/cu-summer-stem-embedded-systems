d5db1a997713bbcc21b5dcab29663ada670c1863
jacobkoziej <jacobkoziej@gmail.com>
2025-07-21 04:18:16 +0000

    df48cf22fbca03fd8e9961caed1e9c9cf7d31305

--- searcher-9aeb6ddf.js
+++ searcher-9aeb6ddf.js
@@ -436,7 +436,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-590d9a71.js',
+                path_to_root + 'searchindex-39bb3466.js',
                 'search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
